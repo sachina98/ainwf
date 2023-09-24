746b3c896faf1a834514ffe7ccfad1b6950313f2
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2023-09-24 21:18:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6518790189:AAH18Rn2TSpRV2cDrFVF-Ruqy9CrChZ-F1s"
-    API_ID = "20717621"
-    API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
-    AUTH_USERS = '5934830127,1085174050'
+    BOT_TOKEN = "6429561862:AAE5oxJxowGuy9pNxRQTLf7kAlx-QAQFxlE"
+    API_ID = "29595682"
+    API_HASH = "acfc97cf83b77467dcc79ac5eea4f7f2"
+    AUTH_USERS = '6674083654'
 
