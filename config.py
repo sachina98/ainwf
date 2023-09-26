0442959f03dc9759f768e7e9bf1224290edb4fee
Stubborn <131737791+stubbornlove@users.noreply.github.com>
2023-09-27 00:18:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6429561862:AAE5oxJxowGuy9pNxRQTLf7kAlx-QAQFxlE"
-    API_ID = "29595682"
-    API_HASH = "acfc97cf83b77467dcc79ac5eea4f7f2"
-    AUTH_USERS = '6674083654'
+    BOT_TOKEN = "6353367506:AAG7mas34CGL2rJoVZyt785w5fAE2dS1nEE"
+    API_ID = "22263567"
+    API_HASH = "258fb6f6cdfa8220b74a4a6753b2ece6"
+    AUTH_USERS = '6446062004'
 
