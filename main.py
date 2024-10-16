6e619a4a7861e83b0cd40195b63a85048ffd0667
sachina98 <97738410+sachina98@users.noreply.github.com>
2024-10-16 13:09:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ prefixes = ["/", "~", "?", "!"]
 plugins = dict(root="plugins")
 if __name__ == "__main__" :
     bot = Client(
-        "StarkBot",
+        "cpdwnbot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
